a445b534dd9ba3d63f7947d204de177ea50678fa
tev1622 <allen.tevyn@gmail.com>
2018-08-27 18:22:06 +0100

    Finished chapter 7

--- src/main/scala/functionalparallelism/Par.scala
+++ src/main/scala/functionalparallelism/Par.scala
@@ -1,6 +1,6 @@
 package functionalparallelism
 
-import java.util.concurrent.{Future, ExecutorService, TimeUnit}
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
 object Par {
 
@@ -18,14 +18,17 @@ object Par {
   //for extracting the resulting value from a parallel computation
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  def fork[A](a: => Par[A]): Par[A] = ???
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call = a(es).get
+    })
 
   def map2[A, B, C](p1: Par[A], p2: Par[B])(f: (A, B) => C): Par[C] = {
     (es: ExecutorService) =>
-      val a = p1(es).get()
-      val b = p2(es).get()
+      val a = p1(es).get() //p1(es) will submit a Callable to the ExecutorService and get back a Future
+    val b = p2(es).get()
 
-    Fut(f(a, b))
+      Fut(f(a, b))
   }
 
   case class Fut[A](a: A) extends Future[A] {
@@ -62,7 +65,7 @@ object Par {
   }
 
   def nonPrimitiveMap[A, B](fa: Par[A])(f: A => B): Par[B] = {
-   es =>
+    es =>
       val a = fa(es).get()
       Fut(f(a))
   }
@@ -91,8 +94,14 @@ object Par {
   def parFilter2[A](l: List[A])(f: A => Boolean): Par[List[A]] =
     parMap(l)(a => if (f(a)) List(a) else Nil)
 
+  def parFilter3[A](l: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pars: List[Par[List[A]]] =
+      l map asyncF((a: A) => if (f(a)) List(a) else List())
+    map(sequence(pars))(_.flatten)
+  }
+
   def sum(as: IndexedSeq[Int]): Par[Int] =
-    if(as.size <= 1) Par.unit(as.headOption getOrElse 0)
+    if (as.size <= 1) Par.unit(as.headOption getOrElse 0)
     else {
       val (l, r) = as.splitAt(as.length / 2)
       map2(sum(l), sum(r))(_ + _)
@@ -101,14 +110,14 @@ object Par {
   def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean =
     p(e).get == p2(e).get
 
-  //identity fucntion
+  //identity function
   def id[A](a: A): A = a
 
   val x = 1
   val y = unit(x)
-  map(unit(x))(id) == unit(id(x)) // subsitiute identity function for f
+  map(unit(x))(id) == unit(id(x)) // substitute identity function for f
   map(unit(x))(id) == unit(x) // simplify
-  map(y)(id) == y // subsitute y for unit(x) on both sides
+  map(y)(id) == y // substitute y for unit(x) on both sides
 
   /**
     * To get some insight into what this new law is saying,
@@ -117,8 +126,71 @@ object Par {
     * - Can you see why this violates the law?
     */
 
+  //EXERCISE 9
+  /**
+    * map(map(y)(g))(f) == map(y)(f compose g)
+    */
+
+  //fork(x) == x  - fork should not affect the result of a parallel computation
+  //fork(x) should 'do the same thing' as x, but asynchronously, in a logical thread separate from the main thread.
 
 
+  def fork_simple[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] { // invoking submit inside a callable
+      def call = a(es).get // we are blocking on the result of what we've submitted
+    })
 
+
+  //EXERCISE 11 - Can you show that any fixed size thread pool can be made to
+  // deadlock given this implementation of fork?
+  fork(fork(fork(y)))
+  // considering if we had a fixed size thread pool executing multiple forks which
+  // will mean there will be another deadlock since our current impl of fork will require at
+  // least three threads in order to complete one fork
+
+  def delay[A](fa: => Par[A]): Par[A] =
+    es => fa(es)
+
+
+  //EXERCISE 14
+  def choice[A](a: Par[Boolean])(ifTrue: Par[A], ifFalse: Par[A]): Par[A] =
+    es => if (a(es).get()) ifTrue(es) else ifFalse(es)
+
+  // alternatively could use run function to extract the result
+  // a(es) == run(es)(a)
+
+  //EXERCISE 15 - to be able to choose between two parallel computations based on the results of a first
+  def choiceN[A](a: Par[Int])(choices: List[Par[A]]): Par[A] =
+    es => {
+      val execPar = run(es)(a).get()
+      run(es)(choices(execPar))
+    }
+
+  def choiceInTermsOfChoiceN[A](a: Par[Boolean])(ifTrue: Par[A], ifFalse: Par[A]): Par[A] =
+    choiceN(map(a)(bool => if (bool) 0 else 1))(List(ifTrue, ifFalse))
+
+  //EXERCISE 16
+  def choiceMap[A, B](a: Par[A])(choices: Map[A, Par[B]]): Par[B] =
+    es => {
+      val parKey = a(es).get()
+      choices(parKey)(es)
+    }
+
+  //EXERCISE 17
+  def flatMap[A, B](a: Par[A])(choices: A => Par[B]): Par[B] =
+    es => {
+      val par = a(es).get()
+      choices(par)(es)
+    }
+
+  def choiceNWithChooser[A](a: Par[Int])(choices: List[Par[A]]): Par[A] =
+    flatMap(a)(int => choices(int))
+
+  def choiceWithChooser[A](a: Par[Boolean])(ifTrue: Par[A], ifFalse: Par[A]): Par[A] =
+    flatMap(a)(bool => if (bool) ifTrue else ifFalse)
+
+  //EXERCISE 18
+  def join[A](a: Par[Par[A]]): Par[A] =
+    es => a(es).get()(es)
 
 }
