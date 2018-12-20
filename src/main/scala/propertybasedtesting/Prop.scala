8a99ba2b61a9ca38556f316ad46bb47860a86e26
tev1622 <allen.tevyn@gmail.com>
2018-12-20 12:19:05 +0000

    Finished chapter 8

--- src/main/scala/propertybasedtesting/Prop.scala
+++ src/main/scala/propertybasedtesting/Prop.scala
@@ -1,5 +1,9 @@
 package propertybasedtesting
 
+import java.util.concurrent.{ExecutorService, Executors}
+
+import functionalparallelism.Par
+import functionalparallelism.Par.Par
 import propertybasedtesting.Prop.{FailedCase, Result, SuccessCount, TestCases}
 import purelyfunctionalstate.{RNG, State}
 
@@ -40,23 +44,7 @@ object Prop {
     }
   }
 
-}
-
-case class Gen[+A](sample: State[RNG, A], exhaustive: Option[Stream[A]]) {
-
-  //  type Gen[A] = State[RNG, A]
-  //type Gen[+A] = (State[RNG, A], List[A])
-
-  def unit[A](a: => A): Gen[A] = Gen(State.unit(a), None)
-
-  def boolean: Gen[Boolean] = Gen(State(RNG.boolean), None)
-
-  def listOf[A](a: Gen[A]): Gen[List[A]] = ???
-
-  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
-    Gen(State.sequence(List.fill(n)(g.sample)), None)
-
-  /*def forAll[A](a: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) => {
+  def forAll[A](a: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) => {
     def go(i: Int, j: Int, s: Stream[Option[A]], onEnd: Int => Result):
     Result =
       if (i == j) Right((Unfalsified, i))
@@ -79,7 +67,7 @@ case class Gen[+A](sample: State[RNG, A], exhaustive: Option[Stream[A]]) {
       case s => s
     }
   }
-  }*/
+  }
 
   def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
     Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
@@ -101,11 +89,67 @@ case class Gen[+A](sample: State[RNG, A], exhaustive: Option[Stream[A]]) {
       "generated an exception: " + e.getMessage + "\n" +
       "stack trace:\n" + e.getStackTrace.mkString("\n")
 
-  def uniform: Gen[Double] =
-    Gen(State(RNG.double), Some(Stream.apply()))
 
-  def choose(i: Double, j: Double): Gen[Double] =
-    Gen(State(RNG.double).map(d => i + d % j), Some(Stream.apply(i)))
+  val ES: ExecutorService = Executors.newCachedThreadPool
+  val p1 = Prop.forAll(Gen.unit(Par.unit(1)))(i =>
+    Par.map(i)(_ + 1)(ES).get == Par.unit(2)(ES).get)
+
+  def check(p: => Boolean): Prop = forAll(unit(()))(_ => p)
+
+  def equal[A](p: Par[A], p2: Par[A]): Par[Boolean] =
+    Par.map2(p,p2)(_ == _)
+
+  val p3: Prop = check {
+    equal (
+      Par.map(Par.unit(1))(_ + 1),
+      Par.unit(2)
+    ) (ES).get
+  }
+
+  val S: Gen[ExecutorService] = Gen.weighted(
+    Gen.choose(1,4).map(Executors.newFixedThreadPool) -> .75,
+    Gen.unit(Executors.newCachedThreadPool) -> .25)
+
+  def forAllPar[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S.map2(g)((_,_))) { case (s,a) => f(a)(s).get }
+
+  def checkPar(p: Par[Boolean]): Prop =
+    forAllPar(Gen.unit(()))(_ => p)
+
+
+  def forAllPar2[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case (s,a) => f(a)(s).get }
+
+  def forAllPar3[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case s ** a => f(a)(s).get }
+
+  val p2 = checkPar {
+    equal (
+      Par.map(Par.unit(1))(_ + 1),
+      Par.unit(2)
+    )
+  }
+
+  val pint: Gen[Par[Nothing]] = Gen.choose(0, 10) map (Par.unit(_))
+  val p4 = forAllPar(pint)(n => equal(Par.map(n)(y => y), n))
+
+}
+
+case class Gen[+A](sample: State[RNG, A], exhaustive: Option[Stream[A]]) {
+
+  //  type Gen[A] = State[RNG, A]
+  //type Gen[+A] = (State[RNG, A], List[A])
+
+  def boolean: Gen[Boolean] = Gen(State(RNG.boolean), None)
+
+  def listOf[A](a: Gen[A]): Gen[List[A]] = ???
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)), None)
+
+  val sortedProp: Prop = forAll(listOf(smallInt)) { l =>
+    l.isEmpty || !l.zip(l.tail).exists {case (a,b) => a > b}
+  }
 
   //EXERCISE 7
   def map[A, B](f: A => B): Gen[B] =
@@ -124,13 +168,6 @@ case class Gen[+A](sample: State[RNG, A], exhaustive: Option[Stream[A]]) {
   //EXERCISE 10
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
     boolean.flatMap(b => if (b) g1 else g2)
-
-  // EXERCISE 11
-  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
-    val g1Prob = g1._2 / (g1._2 + g2._2)
-
-    Gen(State(RNG.double).flatMap(d => if (d < g1Prob) g1._1.sample else g2._1.sample), None)
-  }
 
   //EXERCISE 13
   def unsized: SGen[A] = SGen(_ => this)
@@ -155,14 +192,14 @@ case class Gen[+A](sample: State[RNG, A], exhaustive: Option[Stream[A]]) {
     SGen(i => g.listOfN(i max 1))
 
   //EXERCISE 20
-  val sortedProp = forAll(listOf(smallInt)) { l =>
-    val ls = l.sorted
-    l.isEmpty || !l.zip(l.tail).exists {case (a,b) => a > b}
-  }
+
+  def **[B](g: Gen[B]): Gen[(A,B)] = (this map2 g)((_, _))
 
 }
 
 object Gen {
+
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a), None)
 
   def choose(start: Int, stopExclusive: Int): Gen[Int] = {
     Gen(State(RNG.positiveInt).map(n => start + n % stopExclusive), None)
@@ -173,6 +210,28 @@ object Gen {
     val num = ???
     ???
   }
+
+  def uniform: Gen[Double] =
+    Gen(State(RNG.double), Some(Stream.apply()))
+
+  def choose(i: Double, j: Double): Gen[Double] =
+    Gen(State(RNG.double).map(d => i + d % j), Some(Stream.apply(i)))
+
+  // EXERCISE 11
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val g1Prob = g1._2 / (g1._2 + g2._2)
+
+    Gen(State(RNG.double).flatMap(d => if (d < g1Prob) g1._1.sample else g2._1.sample), None)
+  }
+
+  object ** {
+    def unapply[A,B](p: (A, B)) = Some(p)
+  }
+
+  def genStringIntFn(g: Gen[Int]): Gen[String => Int] = g.map(i => _ => i) // look into the map function - it can infer based on the return type
+
+  def genTypeIntFn[F[_]](g: Gen[Int])(f: F => Int): Gen[F => Int] = g.map(_ => f)
+
 }
 
 trait SGen[+A]
@@ -188,9 +247,6 @@ case class SGen[+A](forSize: Int => Gen[A]) {
 }
 
 trait Status
-
 case object Exhausted extends Status
-
 case object Proven extends Status
-
 case object Unfalsified extends Status
