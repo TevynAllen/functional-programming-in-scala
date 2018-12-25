9bdf601d38a63bf83c6e5f365b2f03fc2082bb6a
tev1622 <allen.tevyn@gmail.com>
2018-12-25 11:38:38 +0000

    Add Laws

--- src/main/scala/parsing/Parsers.scala
+++ src/main/scala/parsing/Parsers.scala
@@ -1,22 +1,29 @@
 package parsing
 
+import propertybasedtesting.{Gen, Prop}
+
 //[_] scala syntax for a type parameter that is itself a type constructor
-trait Parsers[ParseError, Parser[+_]] { // a type constructor type argument
+trait Parsers[ParseError, Parser[+ _]] { // a type constructor type argument
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def char(c: Char): Parser[Char]
+
+  def char(c: Char): Parser[Char] = string(c.toString) map (_.chatAt(0))
+
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+
   //combinator to recognise two possible string values
   def orString(s1: String, s2: String): Parser[String]
 
   //choosing between two parsers, regardless of their result type
   def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
 
   case class ParserOps[A](p: Parser[A]) {
-    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2) // use self to explicitly disambiguate reference to the or method on the trait
-    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2) // use self to explicitly disambiguate reference to the or method on the trait
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
   }
 
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
@@ -26,10 +33,19 @@ trait Parsers[ParseError, Parser[+_]] { // a type constructor type argument
   // For instance, given "aa", the parser results in 2, given "" or "b123" (a string not starting with 'a'),
   // it results in 0, and so on.
   def noOfChars(p: Parser[Char]): Parser[Int]
+
   def many[A](p: Parser[A]): Parser[List[A]]
 
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B] = ???
 
+  object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      Prop.forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
+
+  }
 
 
 }
