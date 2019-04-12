ecbb6602540824ab83b35c3cf6ad404562828bfa
tev1622 <allen.tevyn@gmail.com>
2019-04-12 15:35:22 +0100

    implement foldable

--- src/main/scala/monoid/Monoid.scala
+++ src/main/scala/monoid/Monoid.scala
@@ -19,12 +19,17 @@ import propertybasedtesting.Prop
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
-//  def foldRight(z: A)(f: (A, A) => A): A
-//  def foldLeft(z: A)(f: (A, A) => A): A
+}
+
+object Monoid {
+
+  //  def foldRight(z: A)(f: (A, A) => A): A
+  //  def foldLeft(z: A)(f: (A, A) => A): A
 
   //string monoid
   val stringMonoid: Monoid[String] = new Monoid[String] {
     def op(a1: String, a2: String): String = a1 + a2
+
     def zero = ""
   }
 
@@ -33,41 +38,48 @@ trait Monoid[A] {
   val t = words.foldLeft(stringMonoid.zero)(stringMonoid.op)
 
   //list concatenation also forms a monoid
-  def listMonoid[A] = new Monoid[List[A]] {
+  def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
     def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
     def zero = Nil
   }
 
   //EXERCISE 1: Give Monoid instances for integer addition and multiplication as well as the Boolean operators
   val intAddition: Monoid[Int] = new Monoid[Int] {
-     def op(a1: Int, a2: Int): Int = a1 + a2
-     def zero: Int = 0
+    def op(a1: Int, a2: Int): Int = a1 + a2
+
+    def zero: Int = 0
   }
 
   val intMultiplication: Monoid[Int] = new Monoid[Int] {
     override def op(a1: Int, a2: Int): Int = a1 * a2
+
     override def zero: Int = 0
   }
 
   val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
     def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
     def zero: Boolean = false
   }
 
   val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
     def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
     def zero: Boolean = true
   }
 
   // EXERCISE 2: Give a Monoid instance for combining Options
   def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
     def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+
     def zero: Option[A] = None
   }
 
   //EXERCISE 3: A function having the same argument and return type is called an endofunction
   def EndoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
     def op(a1: A => A, a2: A => A): A => A = a1 andThen a2
+
     def zero: A => A = (a: A) => a
   }
 
@@ -76,8 +88,8 @@ trait Monoid[A] {
   ///EXERCISE 5 - Write a monoid instance for String that inserts spaces between words unless there already is one, and trims spaces off the ends of the result
   def wordsMonoid(s: String): Monoid[String] = new Monoid[String] {
     def op(a1: String, a2: String): String =
-      if(a1 == zero) a2
-      else if(a2 == zero) a1
+      if (a1 == zero) a2
+      else if (a2 == zero) a1
       else a1.trim + " " + a2.trim
 
     def zero: String = " "
@@ -99,6 +111,7 @@ trait Monoid[A] {
     foldMap(as, EndoMonoid[B])(a => b => f(a, b))(z)
 
   sealed trait WC
+
   case class Stub(chars: String) extends WC // we have not seen any complete words yet
   case class Part(lStub: String, words: Int, rStub: String) extends WC //
 
@@ -109,12 +122,42 @@ trait Monoid[A] {
         case (Stub(a), Stub(aa)) => Stub(a + aa)
         case (Stub(a), Part(b, i, c)) => Part(a + b, i, c)
         case (Part(b, i, c), Stub(a)) => Part(b, i, c + a)
-        case (Part(a, i, c), Part(aa, ii, cc)) =>  Part(a, i + (if((c + aa).isEmpty) 0 else 1) + ii, cc)
-    }
+        case (Part(a, i, c), Part(aa, ii, cc)) => Part(a, i + (if ((c + aa).isEmpty) 0 else 1) + ii, cc)
+      }
 
     def zero: WC = Stub("")
   }
 
   //EXERCISE 10
+  def count(s: String): Int = ???
+
+  //EXERCISE 11
+  def foldMapV[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
+    if (v.isEmpty)
+      m.zero
+    else if (v.length == 1)
+      f(v(0))
+    else {
+      val (l, r) = v.splitAt(v.length / 2)
+      m.op(foldMapV(l, m)(f), foldMapV(r, m)(f))
+    }
+  }
+
+}
+
+trait Foldable[F[_]] {
+  import monoid.Monoid._
+
+  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B =
+    foldMap(as)(a => (b: B) => f(a, b))(EndoMonoid[B])(z)
+
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B =
+    foldMap(as)(a => (b: B) => f(b, a))(EndoMonoid[B])(z)
+
+  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+    foldLeft(as)(mb.zero)((b, a) => mb.op(b, f(a)))
+
+  def concatenate[A](as: F[A])(m: Monoid[A]): A =
+    foldLeft(as)(m.zero)(m.op)
 
 }
