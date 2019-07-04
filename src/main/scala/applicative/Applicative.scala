b11e81f0ddbd68243061c56c46aae9e0f69c637e
tev1622 <allen.tevyn@gmail.com>
2019-07-04 16:24:07 +0100

    Add traverse exercise

--- src/main/scala/applicative/Applicative.scala
+++ src/main/scala/applicative/Applicative.scala
@@ -2,7 +2,8 @@ package applicative
 
 import java.util.Date
 
-import monads.Functor
+import monads.{Functor, Monad}
+import monoid.{Foldable, Monoid}
 
 /**
   * When using Monads we lose some compositionality.
@@ -24,6 +25,7 @@ trait Applicative[F[_]] extends Functor[F] {
 
   override def map[A, B](fa: F[A])(f: A => B): F[B] =
     map2(fa, unit(f))((a, b) => b(a))
+
   //simplified into map2(a, unit(f))(_(_))
 
   def apply[A, B](oa: Option[A])(oab: Option[A => B]): Option[B] =
@@ -32,10 +34,27 @@ trait Applicative[F[_]] extends Functor[F] {
       case _ => None
     }
 
-  val v: F[List[Int]] = unit(List(1))
-  val a = apply(unit(x => x))(v) == v
+  def product[G[_]](G: Applicative[G]): Applicative[({type f[x] = (F[x], G[x])})#f] = {
+    val self = this
+    new Applicative[({type f[x] = (F[x], G[x])})#f] {
 
-  val aa: F[List[Int]] = map(v)(x => x)
+      def unit[A](a: => A): (F[A], G[A]) = (self.unit(a), G.unit(a))
+
+      override def apply[A, B](fab: (F[A => B], G[A => B]))(fa: (F[A], G[A])): (F[B], G[B]) =
+        (self.apply(fab._1)(fa._1), G.apply(fab._2)(fa._2))
+
+    }
+  }
+
+  def compose[G[_]](G: Applicative[G]): Applicative[({type f[x] = F[G[x]]})#f] = {
+    val self = this
+    new Applicative[({type f[x] = F[G[x]]})#f] {
+      override def unit[A](a: A): F[G[A]] = self.unit(G.unit(a))
+
+      override def map2[A, B, C](fa: F[G[A]], fb: F[G[B]])(f: (A, B) => C): F[G[C]] =
+        self.map2(fa, fb)((a, b) => G.map2(a, b)(f))
+    }
+  }
 
   /** Difference between functors and applicative functors
     *
@@ -73,6 +92,10 @@ trait Applicative[F[_]] extends Functor[F] {
   def factor[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
     map2(fa, fb)((a, b) => (a, b))
 
+  def sequenceMap[K, V](ofa: Map[K, F[V]]): F[Map[K, V]] =
+  ofa.foldRight(unit(Map.empty[K, V])) { case ((k, f), acc) =>
+    map2(acc, f)((a, v) => a + (k -> v))
+  }
 }
 
 sealed trait Validation[+E, +A]
@@ -131,7 +154,95 @@ object Applicative {
     * -- The should obey the functor law - map(v)(x => x) == v which is the same applicative law of identity -
     * -- 1. The law of identity where the unit preserve identities for instance - apply(unit(x => x))(v) == v
     *
-    * - 2. composition law - preserve function composition
+    * - 2. composition law - preserve function composition and using apply and unit
+    * we can lift function composition in two different ways
+    *
+    * f - F[A => B]
+    * g - F[B => C]
+    * x - F[A]
+    *
+    * apply(apply(apply(unit(a => b => c => a(b(c)))))(g))(x) == apply(f)(apply(g)(x))
+    * they are both applying g to x then f to the result of that
+    *
+    *  3. homomorphism and interchange law establishes the relationship between unit and apply
+    *
+    *  3.1 - homomorphism law states that passing a function and a value through unit is the same
+    * as passing the result of regular application through unit
+    *
+    * apply(unit(f))(unit(x)) == unit(f(x))
+    *
+    * passing a function to a unit and then apply is the same as calling map
+    *
+    * map(unit(x))(f) == unit(f(x))
+    *
+    *
+    *  3.2 - the interchange law states that unit should have the same effect whether applied to the first or second argument of apply
+    *
+    * apply(u)(unit(y)) == apply(unit(_(y)))(u)
+    *
+    * it can be stated in a different way showing a relationship between map2 and map
+    *
+    * map2(u, unit(y))(_(_)) == map(u)(_(y))
+    *
+    * ________
+    *
+    * Overall laws are sanity checks that proves that the algerbas work in the way we would expect
+    *
     */
+
+}
+
+trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
+  //traverse preserves the original structure
+  def traverse[M[_]: Applicative, A, B](fa: F[A])(f: A => M[B]): M[F[B]]
+    ///sequence(map(fa)(f))
+
+  def sequence[M[_]: Applicative, A](fma: F[M[A]]): M[F[A]] =
+    traverse(fma)(ma => ma)
+
+  type Id[A] = A
+
+  val idMonad: Monad[Id] = new Monad[Id] {
+    def unit[A](a: => A) = a
+    override def flatMap[A,B](a: A)(f: A => B): B = f(a)
+  }
+
+ override def map[A,B](fa: F[A])(f: A => B): F[B] =
+    traverse[Id, A, B](fa)(f)(idMonad)
+
+  type Const[A, B] = A
+
+  implicit def monoidApplicative[M](M: Monoid[M]) = new Applicative[({ type f[x] = Const[M, x]})#f] {
+    override def unit[A](a: A): Const[M, A] = M.zero
+
+    override def apply[A, B](m1: M)(m2: M): Const[M, B] = M.op(m1, m2)
+
+  }
+
+  override def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+    traverse[({type f[x] = Const[B, x]})#f, A, Nothing](as)(f)(monoidApplicative(mb))
+}
+
+case class Tree[+A](head: A, tail: List[Tree[A]])
+
+object Traverse {
+
+  val traverseList: Traverse[List] = new Traverse[List] {
+    override def traverse[M[_], A, B](fa: List[A])(f: A => M[B])(implicit M: Applicative[M]): M[List[B]] =
+      fa.foldLeft(M.unit(List[B]()))((acc, la) => M.map2(f(la), acc)((b, l) => b :: l))
+  }
+
+  val traverseOption: Traverse[Option] = new Traverse[Option] {
+    override def traverse[M[_], A, B](fa: Option[A])(f: A => M[B])(implicit M: Applicative[M]): M[Option[B]] =
+      fa match {
+        case Some(v) => M.map(f(v))(a => Some(a))
+        case None => M.unit(None)
+      }
+  }
+
+  val traverseTree: Traverse[Tree] = new Traverse[Tree] {
+    override def traverse[M[_], A, B](fa: Tree[A])(f: A => M[B])(implicit M: Applicative[M]): M[Tree[B]] =
+      M.map2(f(fa.head), traverseList.traverse(fa.tail)(a => traverse(a)(f)))((a, b) => Tree(a, b))
+  }
 
 }
